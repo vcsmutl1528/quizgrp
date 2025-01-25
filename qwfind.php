9e78ad2b6d2a2c02f56e771ef3cd14913e08ed4f
vcsmutl1528 <58227567+vcsmutl1528@users.noreply.github.com>
2025-01-25 11:00:15 +0300

    Add files via upload

--- qwfind.php
+++ qwfind.php
@@ -2,6 +2,7 @@
 <?php
 
 $f_win = strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' ? true : false;
+$f_mb = extension_loaded('mbstring');
 
 $words = array();
 $quizfile = false;
@@ -16,10 +17,9 @@ for ($i=1; $i<$argc; $i++)
 			{
 			case 'f': if (strlen($sc)>1)
 					$quizfile = substr($sc, 1);
-				else
-					$quizfile = $argv[++$i];
+				else	$quizfile = $argv[++$i];
 				goto cont;
-			case 'h': show_help(); exit(0); break;
+			case 'h': show_help(); exit(0);
 			default: echo "Warning: Unknown key '", substr($sc,0,1), "'\n";
 			}
 			$sc = substr ($sc, 1);
@@ -30,8 +30,7 @@ cont:	continue;
 }
 
 if (count($words) < 1) {
-	show_help();
-	exit(0);
+	show_help(); exit(0);
 }
 
 function show_help()
@@ -54,19 +53,15 @@ while (!feof($f))
 	$s = fgets ($f);
 	$s = rtrim ($s, "\n\r");
 	$strim = trim ($s);
-	if (strlen ($strim) == 0)		// Строка пуста
-		continue;
+	if (strlen ($strim) == 0) continue;
 	$ar = explode ('|', $s);
-	if (count ($ar) < 2)			// Разделитель вопроса отсутствует
-		continue;
+	if (count ($ar) < 2) continue;
 	$q = $ar [0];
 	$a = $ar [1];
 	$qtrim = trim ($q);
-	if (strlen ($qtrim) == 0)		// Строка вопроса пуста
-		continue;
+	if (strlen ($qtrim) == 0) continue;
 	$atrim = trim ($a);
-	if (strlen ($atrim) == 0)		// Строка ответа пуста
-		continue;
+	if (strlen ($atrim) == 0) continue;
 	foreach ($words as $word) {
 		if (strcasecmp($atrim, $word) == 0)
 			echo con_cvt($atrim), ": ", con_cvt($qtrim), "\n";
@@ -75,15 +70,14 @@ while (!feof($f))
 fclose($f);
 
 function con_cvt($s) {
-	global $f_win;
-	if ($f_win)	return convert_cyr_string($s, 'w', 'a');
-	return mb_convert_encoding($s, 'UTF-8', 'CP1251');
+	global $f_win, $f_mb;
+	if (!$f_mb && $f_win) return convert_cyr_string($s, 'w', 'a');
+	return mb_convert_encoding($s, $f_win ? 'CP866' : 'UTF-8', 'CP1251');
 }
 
 function cvt_con($s) {
 	global $f_win;
-	if (!$f_win) return mb_convert_encoding($s, 'CP1251', 'UTF-8');
-	return $s;
+	return !$f_win ? mb_convert_encoding($s, 'CP1251', 'UTF-8') : $s;
 }
 
 ?>
